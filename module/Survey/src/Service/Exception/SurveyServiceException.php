5761b7fcb3a00ec1c495d246738e24b94822505c
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-06-27 16:44:01 +0000

    - Expand methods to populate a Survey with Question and QuestionOption instances - New Exception classes to for SurveyService if provided data is invalid

---
+++ module/Survey/src/Service/Exception/SurveyServiceException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arp\Survey\Service\Exception;
+
+class SurveyServiceException extends \RuntimeException
+{
+}
--- module/Survey/src/Service/SurveyService.php
+++ module/Survey/src/Service/SurveyService.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Arp\Survey\Service;
 
+use Arp\Survey\Entity\QuestionOption;
+use Arp\Survey\Entity\Survey;
+use Arp\Survey\Entity\SurveyQuestion;
+use Arp\Survey\Entity\SurveyResponse;
+use Arp\Survey\Service\Exception\InvalidArgumentException;
+
 class SurveyService
 {
     /**
@@ -30,5 +36,85 @@ class SurveyService
         $this->config = $config;
     }
 
+    public function createSurvey(): Survey
+    {
+        return new Survey(
+            $this->createQuestions()
+        );
+    }
 
+    /**
+     * Create a collection of SurveyQuestion instances from the provided $config
+     *
+     * @param array $config
+     *
+     * @return array<SurveyQuestion>
+     */
+    private function createQuestions(array $config): array
+    {
+        $questions = [];
+        foreach ($config as $id => $questionData) {
+            $questions[$id] = $this->createQuestion($id, $questionData);
+        }
+
+        return $questions;
+    }
+
+    /**
+     * Create a single SurveyQuestion instance and populate it with the provided $questionData
+     *
+     * @param int   $id
+     * @param array $questionData
+     *
+     * @return SurveyQuestion
+     */
+    private function createQuestion(int $id, array $questionData): SurveyQuestion
+    {
+        if (empty($questionData['title'])) {
+            throw new InvalidArgumentException(
+                sprintf('The required \'title\' configuration value is missing for question \'%d\'', $id)
+            );
+        }
+
+        if (empty($questionData['type'])) {
+            throw new InvalidArgumentException(
+                sprintf('The required \'type\' configuration value is missing for question \'%d\'', $id)
+            );
+        }
+
+        if (empty($questionData['page'])) {
+            $questionData['page'] = 1;
+        }
+
+        $question = new SurveyQuestion(
+            $id,
+            $questionData['title'],
+            $questionData['title'],
+            $questionData['page']
+        );
+
+        if (!empty($questionData['options'])) {
+            $question->setOptions($this->createQuestionOptions($questionData['options']));
+        }
+
+        return $question;
+    }
+
+    /**
+     * Create a collection of question options from the provided $data
+     *
+     * @param array $data
+     *
+     * @return array<QuestionOption>
+     */
+    public function createQuestionOptions(array $data): array
+    {
+
+    }
+
+
+    public function saveSurveyResponse(SurveyResponse $response): bool
+    {
+
+    }
 }
