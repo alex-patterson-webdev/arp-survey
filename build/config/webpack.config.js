3584b25da864e5fba91374d19896033bad62fd1e
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-06-27 15:37:43 +0000

    Basic webpack configurations with bootstrap 4

---
+++ build/config/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+const BASE_PATH = path.resolve(__dirname, '..', '..');
+const PUBLIC_PATH = path.resolve(BASE_PATH, 'public', 'dist');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(BASE_PATH, 'build', 'assets', 'survey.js'),
+  output: {
+    path: PUBLIC_PATH,
+    filename: "app.js"
+  },
+  "watch" : true,
+  "module": {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "img"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "survey.css"
+    }),
+  ]
+};
