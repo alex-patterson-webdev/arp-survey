ba660fcc821062e82ab224e70870e04792beaaf7
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-06-27 19:20:19 +0000

    Add resetAction() to allow session data to be cleared at the end when complete

--- module/Survey/src/Controller/HomeController.php
+++ module/Survey/src/Controller/HomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Arp\Survey\Controller;
 
+use Laminas\Http\PhpEnvironment\Request;
+use Laminas\Http\Response;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 
@@ -12,10 +14,17 @@ class HomeController extends AbstractActionController
     /**
      * Render the survey home page
      *
-     * @return ViewModel
+     * @return ViewModel|Response
      */
-    public function indexAction(): ViewModel
+    public function indexAction()
     {
+        /** @var Request $request */
+        $request = $this->request;
+
+        if ($request->getQuery('start')) {
+            return $this->redirect()->toRoute('survey');
+        }
+
         $view = new ViewModel();
         $view->setTemplate('survey/home/index');
         return $view;
--- module/Survey/src/Controller/SurveyController.php
+++ module/Survey/src/Controller/SurveyController.php
@@ -44,21 +44,23 @@ class SurveyController extends AbstractActionController
     }
 
     /**
+     * Handle the rendering of the survey questions
+     *
      * @return ViewModel|Response
      */
     public function indexAction()
     {
         $survey = $this->surveyService->createSurvey();
         $response = $this->responseService->loadResponse($survey);
-
         $page = (int)$this->params()->fromRoute('page');
-        $form = $this->surveyForm;
+
+        $form = $this->surveyForm->createElements($survey, $page, $response);
 
         $request = $this->getRequest();
         if ($request->isPost()) {
-            $form->setData($this->params()->fromPost());
+            $form->setData($request->getPost());
 
-            if ($form->isValid() && $this->responseService->save($response)) {
+            if ($form->isValid() && $this->responseService->save($survey, $form->getData())) {
                 if ($page < 3) {
                     return $this->redirect()->toRoute('survey', ['page' => ++$page]);
                 }
@@ -72,11 +74,28 @@ class SurveyController extends AbstractActionController
     }
 
     /**
+     * Allow a new survey to be completed, resetting any existing responses
+     *
+     * @return Response
+     */
+    public function resetAction(): Response
+    {
+        $this->responseService->clearResponse();
+
+        return $this->redirect()->toRoute('survey');
+    }
+
+    /**
+     * Display the survey completion page
+     *
      * @return ViewModel
      */
     public function completeAction(): ViewModel
     {
-        $view = new ViewModel();
+        $survey = $this->surveyService->createSurvey();
+        $response = $this->responseService->loadResponse($survey);
+
+        $view = new ViewModel(['response' => $response]);
         $view->setTemplate('survey/survey/complete');
         return $view;
     }
